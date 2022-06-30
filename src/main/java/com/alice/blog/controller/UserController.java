b98735e8a268988689d0c3ff1f32e9c4b16b80c2
kltu8790 <kltu8790@hknu.ac.kr>
2022-06-30 23:34:19 +0900

    메인화면, 로그인, 회원가입 화면 만들기

---
+++ src/main/java/com/alice/blog/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.alice.blog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+	
+	@GetMapping("/user/joinForm")
+	public String joinForm() {
+		
+		return"user/joinForm";
+		
+	}
+
+}
